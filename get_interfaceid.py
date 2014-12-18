7c61cb8964026bdc61352b2a64b7e3fd60baa010
xluren <xluren@126.com>
2014-12-18 13:16:56 +0800

    add a new demo

--- get_interfaceid.py
+++ get_interfaceid.py
@@ -1,18 +1,16 @@
 from pyzabbix import zabbixapi
 import json
 url="http://10.210.71.145/zabbix/api_jsonrpc.php"
-#log in 
 zb=zabbixapi(url=url,user="admin",password="zabbix")
 response=zb.host.get(
         {
-            "output":"extend",
+            "output":"interfaceid",
             "filter": 
             {
                 "host":"all-summary"
-            }
+            },
+            "selectInterfaces":["interfaceid"]
         })
 print json.dumps(json.loads(response.text),indent=2)
-
-response=zb.host.get({"output": ["extend","hostid"],"filter": {"host": ["all-summary"]}})
-
-print json.dumps(json.loads(response.text),indent=2)
+interfaceid=json.loads(response.text)["result"][0]["interfaces"][0]["interfaceid"]
+print interfaceid
